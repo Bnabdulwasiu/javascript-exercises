cbe46129b855c22e9882f7faea1abdf1cbeaa37f
Bnabdulwasiu <abdulrahmanabdulwasiu111@gmail.com>
2023-08-25 17:31:34 +0100

    Rounded up tests for exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num<0) {
+        return "ERROR"
+    }
+    var final_string = ""
+    for(let i=0; i<num; i++) {
+        final_string+=string;
+    }
+    return final_string
 };
 
 // Do not edit below this line
